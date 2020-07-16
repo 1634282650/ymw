fbe9f720f085f1332eedf0b72e0a692bf4eb6906
admin <1634282650@qq.com>
2020-07-16 16:25:21 +0800

    测试test 中的t文件

--- ymw/java/test/t.java
+++ ymw/java/test/t.java
@@ -8,7 +8,7 @@ public class t {
     public static void main(String[] args) throws SQLException {
         BaseDao b=new BaseDao(DatabaseUtil.getConnection());
         try {
-int i=0;
+            int i=0;
             b.executeUpdate("UPDATE `easybuy_news` SET `content`='1111111会员特惠月开始了1'\n" +
                     "\n" +
                     "WHERE id=?;",597);
