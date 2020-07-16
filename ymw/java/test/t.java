1571f8b845a1ef81af295770da0634ff5b261207
admin <1634282650@qq.com>
2020-07-16 15:30:08 +0800

    测试

---
+++ ymw/java/test/t.java
@@ -0,0 +1,18 @@
+import dao.BaseDao;
+import util.DatabaseUtil;
+
+import java.sql.SQLException;
+
+public class t {
+
+    public static void main(String[] args) throws SQLException {
+        BaseDao b=new BaseDao(DatabaseUtil.getConnection());
+        try {
+            b.executeUpdate("UPDATE `easybuy_news` SET `content`='1111111会员特惠月开始了1'\n" +
+                    "\n" +
+                    "WHERE id=?;",597);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
