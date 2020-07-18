bd242648992e0228f125d3ca06937bd417c92542
Dec <1084982864@qq.com>
2020-07-18 12:14:57 +0800

    垃圾徐英杰的EasybuyNews实体类代码规范的修正

--- ymw/java/entity/EasybuyNews.java
+++ ymw/java/entity/EasybuyNews.java
@@ -20,35 +20,59 @@ public class EasybuyNews {
      *  新闻创建时间
      */
     private String createTime;
-
+    /**
+     *  获取id
+     * @return id
+     */
     public Integer getId() {
         return id;
     }
-
+    /**
+     *  设置主键id
+     * @param id 主键id
+     */
     public void setId(Integer id) {
         this.id = id;
     }
-
+    /**
+     *  获取标题
+     * @return 标题
+     */
     public String getTitle() {
         return title;
     }
-
+    /**
+     *  设置标题
+     * @param title 标题
+     */
     public void setTitle(String title) {
         this.title = title;
     }
-
+    /**
+     *  获得新闻内容
+     * @return 新闻内容
+     */
     public String getContent() {
         return content;
     }
-
+    /**
+     *  设置新闻内容
+     * @param content 新闻内容
+     */
     public void setContent(String content) {
         this.content = content;
     }
-
+    /**
+     *  获得新闻创建时间
+     * @return 新闻创建时间
+     */
     public String getCreateTime() {
         return createTime;
     }
-
+    /**
+     *  设置新闻创建时间
+     * @param createTime 需要设置的新闻创建时间
+     */
     public void setCreateTime(String createTime) {
         this.createTime = createTime;
     }
